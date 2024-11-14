51a650a466916459061b094ef0a56e1966cbb988
GengiuRobert <fncrobert3@gmail.com>
2024-11-14 13:43:05 +0200

    admin dashboard form to create user

--- frontendpandacar/panda-car-react/src/Components/AdminDashbord/AdminDashbord.jsx
+++ frontendpandacar/panda-car-react/src/Components/AdminDashbord/AdminDashbord.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import './AdminDashbord.css';
-import pandaIcon from '../Assets/panda3.png'; // Adjust path if necessary
+import pandaIcon from '../Assets/panda3.png';
 
 const AdminDashbord = () => {
     const [data, setData] = useState({
@@ -23,6 +23,34 @@ const AdminDashbord = () => {
         cars: false,
         availabilities: false
     });
+
+    const [createError, setCreateError] = useState(null);
+    const [createSuccess, setCreateSuccess] = useState(null);
+
+    const [showCreateUserForm, setShowCreateUserForm] = useState(false);
+
+    const [newUser, setNewUser] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        isAdmin: false,
+        dateOfBirth: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const handleInputChange = (event) => {
+        const { name, value, type, checked } = event.target;
+        setNewUser(prev => ({
+            ...prev,
+            [name]: type === 'checkbox' ? checked : value
+        }));
+    };
+
+    const toggleCreateUserForm = () => {
+        setShowCreateUserForm(prev => !prev);
+    };
 
     const fetchAPI = async (endpoint, dataKey) => {
         setLoading(prev => ({ ...prev, [dataKey]: true }));
@@ -64,6 +92,62 @@ const AdminDashbord = () => {
             fetchAPI(dataKey, dataKey);
         }
     };
+
+    const handleCreateUser = async (event) => {
+        event.preventDefault();
+        setCreateError(null);
+        setCreateSuccess(null);
+        try {
+
+            if (newUser.password !== newUser.confirmPassword) {
+                setCreateError('Passwords do not match');
+                return;
+            }
+
+            const response = await fetch('http://127.0.0.1:8000/api/users/create', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    first_name: newUser.firstName,
+                    last_name: newUser.lastName,
+                    email: newUser.email,
+                    phone_number: newUser.phoneNumber,
+                    password: newUser.password,
+                    is_admin: newUser.isAdmin,
+                    date_of_birth: newUser.dateOfBirth
+                })
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                console.error('Error creating user:', errorData);
+                setCreateError('Error creating user. Please check the input data.');
+                return;
+            }
+
+            setNewUser({
+                firstName: '',
+                lastName: '',
+                email: '',
+                phoneNumber: '',
+                isStaff: false,
+                isAdmin: false,
+                dateOfBirth: '',
+                password: '',
+                confirmPassword: ''
+            });
+
+            setCreateSuccess('User created')
+
+        } catch (error) {
+            setCreateError('Failed to create user');
+            console.log(error);
+        }
+    }
+
 
     return (
         <div className="contentAdmin">
@@ -168,9 +252,94 @@ const AdminDashbord = () => {
                         </>
                     )}
                 </div>
+
+                {/* Toggle Button for User Creation Form */}
+                <div className="section" id="toggleCreateUserFormDiv">
+                    <button onClick={toggleCreateUserForm} className="toggle-create-user-form-btn">
+                        {showCreateUserForm ? 'Hide Create User Form' : 'Show Create User Form'}
+                    </button>
+                </div>
+
+                {/* User Creation Form */}
+                {showCreateUserForm && (
+                    <div className="section" id="createUserDiv">
+                        <h2>Create New User</h2>
+                        <form className="create-user-form" onSubmit={handleCreateUser}>
+                            <input
+                                type="text"
+                                name="firstName"
+                                placeholder="First Name"
+                                value={newUser.firstName}
+                                onChange={handleInputChange}
+                                required
+                            />
+                            <input
+                                type="text"
+                                name="lastName"
+                                placeholder="Last Name"
+                                value={newUser.lastName}
+                                onChange={handleInputChange}
+                                required
+                            />
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Email"
+                                value={newUser.email}
+                                onChange={handleInputChange}
+                                required
+                            />
+                            <input
+                                type="text"
+                                name="phoneNumber"
+                                placeholder="Phone Number"
+                                required
+                                value={newUser.phoneNumber}
+                                onChange={handleInputChange}
+                            />
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    name="isAdmin"
+                                    checked={newUser.isAdmin}
+                                    onChange={handleInputChange}
+                                />
+                                Is Admin
+                            </label>
+                            <input
+                                type="date"
+                                name="dateOfBirth"
+                                placeholder="Date of Birth"
+                                required
+                                value={newUser.dateOfBirth}
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                type="text"
+                                name="password"
+                                placeholder="Password"
+                                value={newUser.password}
+                                onChange={handleInputChange}
+                                required
+                            />
+                            <input
+                                type="text"
+                                name="confirmPassword"
+                                placeholder="Confirm Password"
+                                value={newUser.confirmPassword}
+                                onChange={handleInputChange}
+                                required
+                            />
+                            <button type="submit">Create User</button>
+                            {createUserError && <p style={{ color: 'red' }}>{createUserError}</p>}
+                            {createUserSuccess && <p style={{ color: 'green' }}>{createUserSuccess}</p>}
+                        </form>
+                    </div>
+                )}
+
+
             </div>
         </div>
     );
 };
-
 export default AdminDashbord;
